79ede4d5e73e9144fb3d6fa3e31f339566d92066
janakiabhinaya <120025482+janakiabhinaya@users.noreply.github.com>
2024-02-25 15:59:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,12 +4,7 @@ function appendtodisplay(input){
   display.value += input;
 }
 function calculate(){
-  try{
     display.value = eval(display.value);
-  }
-  catch(error){
-      display.value = "Error";
-  }
 }
 function deletefn(){
   display.value = display.value.substring(0,display.value.length-1);
